7ba9ad3182db3ec978a91040e07f7522c57fa336
kjm541113 <kjm541006@naver.com>
2021-08-19 17:02:34 +0900

    todos_saving

--- App/js/todo.js
+++ App/js/todo.js
@@ -2,6 +2,14 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos";
+
+const todos = [];
+
+function saveTodos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
 function deleteTodo(event){
     const li = event.target.parentElement;
     li.remove();
@@ -24,8 +32,17 @@ function handleTodoSubmit(event){
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
+    todos.push(newTodo);
     paintTodo(newTodo);
+    saveTodos();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
 
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+console.log(savedTodos);
+if(savedTodos){
+    const parsedTodos = JSON.parse(savedTodos);
+    parsedTodos.forEach(item => console.log("this is the turn of", item));
+}
